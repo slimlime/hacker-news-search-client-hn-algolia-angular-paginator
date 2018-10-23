889c6d5d2636fe83da91aa66a0c9c739bfe29db9
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-10-24 08:39:58 +1000

    :heavy_plus_sign::construction: Add search-result-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchResultItemComponent } from './search-result-item/search-result-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchResultItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/search-result-item/search-result-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-result-item',
+  templateUrl: './search-result-item.component.html',
+  styleUrls: ['./search-result-item.component.css']
+})
+export class SearchResultItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
