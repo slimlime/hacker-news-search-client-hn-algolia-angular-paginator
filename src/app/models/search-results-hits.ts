b8cdac5c522ba1fe4932a335721488e3d0dcec3f
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-10-25 05:42:52 +1000

    :bulb: Search results usage and data model refined. Docs

--- src/app/models/search-results-hits.ts
+++ src/app/models/search-results-hits.ts
@@ -3,7 +3,7 @@ import { SearchResultByTopic } from './search-result-topic';
  * @Author            : Samuel Lim
  * @Date              : 2018-10-24 17: 58: 53
  * @Last Modified by  : slimlime
- * @Last Modified time: 2018-10-25 05: 31: 37
+ * @Last Modified time: 2018-10-25 05: 39: 02
  */
 
 /**
@@ -16,28 +16,89 @@ import { SearchResultByTopic } from './search-result-topic';
  */
 export interface SearchHits {
 
+    /**
+     * Whether search hits count is exhaustive (true) or approximate?
+     *
+     * @type {boolean}
+     * @memberof SearchHits
+     */
     readonly exhaustiveNbHits: boolean;
 
     /**
      * An array of the search result items that represent the stories/comments
      * and other possible hn article item types with their associated metadata.
-     *
+     * 
      * @type {SearchResultByTopic[]}
      * @memberof SearchHits
      */
     readonly searchResultItems: SearchResultByTopic[];
 
+    /**
+     * Hacker News Algolia REST Search API recommended method for pagination.
+     * @see `https://www.algolia.com/doc/guides/searching/pagination/` API docs
+     * 
+     * @example 20
+     * Minimum 0, Maximum 1000.
+     * @type {number}
+     * @memberof SearchHits
+     */
     readonly hitsPerPage: number;  // Default 20
 
+    /**
+     * Number of hits found for the given search query parameters.
+     * 
+     * @example 18701
+     * @type {number}
+     * @memberof SearchHits
+     */
     readonly nbHits: number;  // Maybe total number of hits.
 
+    /**
+     * Number of pages.
+     * Influenced by number of hits and hitsPerPage
+     * 
+     * Seems to hit a soft limit of 50 pages.
+     *
+     * @type {number}
+     * @memberof SearchHits
+     */
     readonly nbPages: number;  // Total number of pages // soft limit 50 pages?
 
+    /**
+     * Virtual page of the results.
+     *
+     * @type {number}
+     * @memberof SearchHits
+     */
     readonly page: number;  // Virtual page of REST API Search
 
+    /**
+     * GET Parameters verified.
+     * 
+     * endpoint parameters returned
+     * May return different to what was queried
+     * e.g. Prefixed default `advancedSyntax=true` `&` `analytics=false`
+     *
+     * @type {string}
+     * @memberof SearchHits
+     */
     readonly params: string;  // GET parameters verified.
 
+    /**
+     * HN Algolia search processing time.
+     * 
+     * @example 27
+     * @type {number}
+     * @memberof SearchHits
+     */
     readonly processingTimeMS: number  // REST API Processing time.
     
+    /**
+     * Query string user search input.
+     *
+     * @example "Deep Learning"
+     * @type {string}
+     * @memberof SearchHits
+     */
     readonly query: string;  // Query string
 }
