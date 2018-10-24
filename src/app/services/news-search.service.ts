b20e43042ae20fa25166d5f739ba3c080a76a329
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-10-24 20:05:37 +1000

    :Sparkles: Initial test query works with reactivity! 20 hits on Deep Learning stories starting from 2016

--- src/app/services/news-search.service.ts
+++ src/app/services/news-search.service.ts
@@ -7,7 +7,8 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
@@ -26,15 +27,33 @@ export class NewsSearchService {
    * @memberof NewsSearchService
    */
   testHNAlgoliaSearch() {
-    const baseSearchUrl = "http://hn.algolia.com/api/v1/search";
+    const baseSearchHNAlgoliaUrl = "http://hn.algolia.com/api/v1/search";
     
     const searchUrlParamKey: string   = "query";
-    const searchUrlParamValue: string = "Deep Learning"
+    const searchUrlParamValue: string = "Deep Learning";
     // this.httpClient.get(url, options, header, param)
-    const httpSearchParams: HttpParams = new HttpParams()
-      .set((searchUrlParamKey), searchUrlParamValue);
+    const httpSearchParams: HttpParams = new HttpParams().set(
+      (searchUrlParamKey), searchUrlParamValue
+    );
     console.log('​NewsSearchService:: testHNAlgoliaSearch() -> httpSearchParams', httpSearchParams);
-    
 
+    /** 
+     * Destructured http options object { headers, params, ...}
+     * Options object parameter doesn't have an interface in Angular :sad: 
+     */
+    const newsObservable: Observable<any> = this.httpClient.get(baseSearchHNAlgoliaUrl, {params: httpSearchParams});
+    console.log('​NewsSearchService:: testHNAlgoliaSearch() -> newsObservable', newsObservable);
+    
+    const newsSubscription: Subscription = newsObservable.subscribe((data: any) => {
+      console.log('​NewsSearchService:: testHNAlgoliaSearch() -> data', data);
+    });
+
+    const newsObsPiped: Observable<any> = newsObservable.pipe(
+      map((data: any) => {
+        console.log('​NewsSearchService:: testHNAlgoliaSearch() -> data', data);
+      })
+    );
+
+    
   }
 }
