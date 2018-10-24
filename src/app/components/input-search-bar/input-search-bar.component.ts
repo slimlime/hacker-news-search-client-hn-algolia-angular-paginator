0ce6275b545719a494f48e108927916de9a2c72d
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-10-24 15:12:12 +1000

    :heavy_plus_sign Outlining InputSearchBarComponent with NewsSearchService

--- src/app/components/input-search-bar/input-search-bar.component.ts
+++ src/app/components/input-search-bar/input-search-bar.component.ts
@@ -1,13 +1,23 @@
+/*
+ * @Author            : Samuel Lim
+ * @Date              : 2018-10-24 15: 11: 23
+ * @Last Modified by  : slimlime
+ * @Last Modified time: 2018-10-24 15: 11: 23
+ */
 import { Component, OnInit } from '@angular/core';
 
+import { NewsSearchService } from './../../services/news-search.service';
+
 @Component({
-  selector: 'app-input-search-bar',
+  selector   : 'app-input-search-bar',
   templateUrl: './input-search-bar.component.html',
-  styleUrls: ['./input-search-bar.component.css']
+  styleUrls  : ['./input-search-bar.component.css']
 })
 export class InputSearchBarComponent implements OnInit {
 
-  constructor() { }
+  constructor( public newsSearchService: NewsSearchService) {
+
+  }
 
   ngOnInit() {
   }
