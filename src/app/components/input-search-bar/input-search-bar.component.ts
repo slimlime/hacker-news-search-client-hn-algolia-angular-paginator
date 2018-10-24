ad955010bee40532c930f7fca40f0f8baf2ccb8c
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-10-25 08:16:36 +1000

    :tada: Search input events work

--- src/app/components/input-search-bar/input-search-bar.component.ts
+++ src/app/components/input-search-bar/input-search-bar.component.ts
@@ -2,7 +2,7 @@
  * @Author            : Samuel Lim
  * @Date              : 2018-10-24 15: 11: 23
  * @Last Modified by  : slimlime
- * @Last Modified time: 2018-10-24 15: 11: 23
+ * @Last Modified time: 2018-10-25 08: 16: 32
  */
 import { Component, OnInit } from '@angular/core';
 
@@ -15,11 +15,27 @@ import { NewsSearchService } from './../../services/news-search.service';
 })
 export class InputSearchBarComponent implements OnInit {
 
+  // @Input @Output vs Shared singleton reactive service. References issue #18
   constructor( public newsSearchService: NewsSearchService) {
 
   }
 
   ngOnInit() {
+    console.log('​InputSearchBarComponent:: ngOnInit()');
   }
 
+  /**
+   * 
+   *
+   * @param {KeyboardEvent} event typecast info for input and event properties.
+   * @memberof InputSearchBarComponent
+   */
+  onSearchInput(inputKeyEvent: KeyboardEvent) {
+    console.log('​InputSearchBarComponent:: onSearchInput() -> inputKeyEvent', inputKeyEvent);
+    const searchInputElementTarget: HTMLInputElement = <HTMLInputElement>inputKeyEvent.target;  // preserving type information
+    const searchInputTopic: string                   = searchInputElementTarget.value;
+    
+    console.log('​InputSearchBarComponent:: onSearchInput() -> searchInput', searchInputTopic);
+
+  }
 }
