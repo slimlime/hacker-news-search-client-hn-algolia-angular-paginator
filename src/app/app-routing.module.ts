7ddf5e6bbb747aeb862e2dee60a4c9f51dbc996d
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-10-24 15:17:26 +1000

    :art: Trailing whitespace, formatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,19 @@
+/*
+ * @Author            : Samuel Lim
+ * @Date              : 2018-10-24 14: 58: 40
+ * @Last Modified by  : slimlime
+ * @Last Modified time: 2018-10-24 15: 16: 51
+ */
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+
+  }
+];
 @NgModule({
   imports: [
     CommonModule
