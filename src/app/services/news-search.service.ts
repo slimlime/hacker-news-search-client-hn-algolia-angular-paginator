d08f0be13db64b76927a8c2d6eb203e3de5ddc5b
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-10-24 19:03:05 +1000

    :construction: WIP Search Service requests logic and data

--- src/app/services/news-search.service.ts
+++ src/app/services/news-search.service.ts
@@ -1,9 +1,19 @@
+/*
+ * @Author            : Samuel Lim
+ * @Date              : 2018-10-24 19: 01: 06
+ * @Last Modified by  : slimlime
+ * @Last Modified time: 2018-10-24 19: 01: 06
+ */
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsSearchService {
 
-  constructor() { }
+  constructor(public httpClient: HttpClient) { 
+
+  }
 }
