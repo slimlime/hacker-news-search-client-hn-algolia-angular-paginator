c994c5d1abd2ee6d4b356cd33dfb738742227253
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-10-24 19:09:00 +1000

    :wrench::heavy_plus_sign: Fixed Angular 6 HttpClient..Module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
+/*
+ * @Author            : Samuel Lim
+ * @Date              : 2018-10-24 19: 06: 35
+ * @Last Modified by  : slimlime
+ * @Last Modified time: 2018-10-24 19: 07: 18
+ */
+
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './/app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { InputSearchBarComponent } from './components/input-search-bar/input-search-bar.component';
 import { SearchResultItemComponent } from './components/search-result-item/search-result-item.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { InputSearchBarComponent } from './components/input-search-bar/input-search-bar.component';
-import { ButtonComponent } from './components/button/button.component';
 
 
 @NgModule({
@@ -19,6 +27,7 @@ import { ButtonComponent } from './components/button/button.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/services/news-search.service.ts
+++ src/app/services/news-search.service.ts
@@ -2,18 +2,24 @@
  * @Author            : Samuel Lim
  * @Date              : 2018-10-24 19: 01: 06
  * @Last Modified by  : slimlime
- * @Last Modified time: 2018-10-24 19: 01: 06
+ * @Last Modified time: 2018-10-24 19: 08: 30
  */
 
 import { Injectable } from '@angular/core';
-import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsSearchService {
 
-  constructor(public httpClient: HttpClient) { 
+  constructor( public httpClient: HttpClient) {
 
+
+  }
+
+  testHNAlgoliaSearch() {
+    // this.httpClient.get(url, options, header, param)
   }
 }
