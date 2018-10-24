aad9503ac6ef5641dc0b5872f52aa5d0e4432f10
MyNameIsSL <30815731+slimlime@users.noreply.github.com>
2018-10-24 18:10:09 +1000

    :bulb: Clarified the data model and hn algolia REST API results

--- src/app/models/search-result-topic.ts
+++ src/app/models/search-result-topic.ts
@@ -2,7 +2,7 @@
  * @Author            : Samuel Lim
  * @Date              : 2018-10-24 15: 16: 15
  * @Last Modified by  : slimlime
- * @Last Modified time: 2018-10-24 15: 46: 25
+ * @Last Modified time: 2018-10-24 18: 09: 35
  */
 
 /**
@@ -18,8 +18,11 @@
  * instead of manual handling the implementation, buffering, loading etc.
  * * Can filter by condition on `created_at_i`, `points`, `num_comments`
  *
+ * List of presumed disregarded attributes for story (returned null)
+ *  *comment_text, story_ID, story_title, story_url, parent_ID,
+ *  *story_text* sometimes null, "" blank string or has text...?
+ * -- TODO: Check query
  *
-
  * @export
  * @interface SearchResultByTopic
  */
@@ -36,7 +39,7 @@ export interface SearchResultByTopic {
     readonly created_at: string;
 
     /**
-     *
+     * Story title.
      *
      * @type {string}
      * @memberof SearchResultByTopic
@@ -44,6 +47,8 @@ export interface SearchResultByTopic {
     readonly title: string;
 
     /**
+     * Url to the article or null for non-story item
+     * Some buggy story-tagged articles return blank string url?
      *
      * Quirky `hn algolia search` REST API http:   //hn.algolia.com/api/v1/search
      *  Returns null story_url if directly searching for stories.
@@ -54,10 +59,16 @@ export interface SearchResultByTopic {
      */
     readonly url: string;
 
+    /**
+     * Author username
+     *
+     * @type {string}
+     * @memberof SearchResultByTopic
+     */
     readonly author: string;
 
     /**
-     *
+     * Article up-/down-vote points
      *
      * @type {number}
      * @memberof SearchResultByTopic
@@ -65,15 +76,15 @@ export interface SearchResultByTopic {
     readonly points: number;
 
     /**
-     *
-     *
+     * Some search results have story text
+     * NOTE: Quirk returned blank string "" and null values for some results.
      * @type {string}
      * @memberof SearchResultByTopic
      */
     readonly story_text: string;
 
     /**
-     *
+     * Comment text body (if comment)
      *
      * @type {string}
      * @memberof SearchResultByTopic
@@ -81,7 +92,7 @@ export interface SearchResultByTopic {
     readonly comment_text: string;
 
     /**
-     *
+     * Number of comments associated with the story.
      *
      * @type {number}
      * @memberof SearchResultByTopic
@@ -89,7 +100,7 @@ export interface SearchResultByTopic {
     readonly num_comments: number;
 
     /**
-     *
+     * A comment's parent story_ID ? (null for an article)
      *
      * @type {number}
      * @memberof SearchResultByTopic
@@ -105,14 +116,14 @@ export interface SearchResultByTopic {
     readonly story_title: string;
 
     /**
-     *
+     * A non-story item's story_url. (Stories have exclusive url attribute)
      *
      * @type {string}
      * @memberof SearchResultByTopic
      */
     readonly story_url: string;
     /**
-     *
+     * A comment's parent story or parent comment ID? (null for an article)
      *
      * @type {number}
      * @memberof SearchResultByTopic
@@ -127,15 +138,18 @@ export interface SearchResultByTopic {
     readonly created_at_i: number;
 
     /**
-     *
-     *
+     * Tags associated with the story.
+     * Strange quirk. Array of 3 string tags with the following combination
+     * * "comment" or item_type, "author_<author/username>", "story_<story_ID>"
+     * * * Where <> angle brackets indicate the other associated attribute value
      * @type {string[]}
      * @memberof SearchResultByTopic
      */
     readonly tags: string[];
 
     /**
-     *
+     * objectID string number seems to indicate the item ID,
+     * This can be verified by using the hn.algolia item ID endpoint search.
      *
      * @type {string}
      * @memberof SearchResultByTopic
